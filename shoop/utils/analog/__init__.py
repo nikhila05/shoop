feaafc4b881b3226c2730fdcd4ff47e78564c38f
Panu Tulimäki <panu.tulimaki@anders.fi>
2016-04-05 22:25:16 -0700

    Add ugettext_laxy to analog

--- shoop/utils/analog/__init__.py
+++ shoop/utils/analog/__init__.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import force_text
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from ._enums import LogEntryKind
 
