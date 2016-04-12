8a00a89e5cb07aef9fcb33bbbeaf0150fdff5040
Tuomas Suutari <tuomas.suutari@shoop.io>
2016-04-12 17:30:07 +0300

    wip: amend tests for the logo addition

--- shoop_tests/admin/test_service_providers.py
+++ shoop_tests/admin/test_service_providers.py
@@ -118,7 +118,7 @@ def test_new_service_provider_form_fields(rf, admin_user, type, extra_inputs):
     an language.
     """
     with override_settings(LANGUAGES=[("en", "en")]):
-        base_inputs = ["name__en", "enabled"]
+        base_inputs = ["name__en", "enabled", "logo"]
         get_default_shop()
         view = ServiceProviderEditView.as_view()
         soup = get_bs_object_for_view(rf.get("?type=%s" % type), view, admin_user)
@@ -144,7 +144,7 @@ def test_service_provide_edit_view(rf, admin_user, sp_model, extra_inputs):
     an language.
     """
     with override_settings(LANGUAGES=[("en", "en")]):
-        base_inputs = ["name__en", "enabled"]
+        base_inputs = ["name__en", "enabled", "logo"]
         get_default_shop()
         view = ServiceProviderEditView.as_view()
         provider_name = "some name"
